0f19b09c4c0a183c930247dd981d28a295e143e4
Davidsb04 <121972361+Davidsb04@users.noreply.github.com>
2023-08-15 22:47:09 -0300

    New commit

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ function calcular(){
     const peso = parseFloat(document.getElementById('peso').value)
     const altura = parseFloat(document.getElementById('altura').value) 
     const resultado = document.querySelector('.res')
+    const classfElement = document.getElementById('classfElement')
 
     if(isNaN(peso) || isNaN(altura)){
         alert('Digite os valores!')
     }else{
         const imc = peso / (altura*altura)
         resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)}`
+        if (imc <= 18.5) {
+            classification = 'Abaixo do Peso'
+        } else if (imc <= 24.9) {
+            classification = 'Peso Ideal'
+        } else if (imc <= 29.9) {
+            classification = 'Levemente Acima do Peso'
+        } else if (imc <= 34.9) {
+            classification = 'Obesidade de Grau I'
+        } else if (imc <= 39.9) {
+            classification = 'Obesidade de Grau II'
+        } else {
+            classification = 'Obesidade de Grau III'
+        }
+
+        classfElement.innerHTML = classification
+        classfElement.style.width = classification.length * 11 + 'px'
+        resultado.style.backgroundColor = '#a5eaff'
+        classfElement.style.backgroundColor = '#a5eaff'
     }
 }
 function formatNumberField(input) {
